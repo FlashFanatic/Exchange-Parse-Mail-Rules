11b72a2fb52af42f72b945ac66ecdcd1e28b089d
Flash Fanatic <121263173+FlashFanatic@users.noreply.github.com>
2023-01-19 14:33:01 -0800

    Add files via upload

---
+++ parse.ps1
@@ -0,0 +1,43 @@
+﻿#use this to install, uncomment and install
+Install-Module ExchangeOnlineManagement
+
+#use this to connect and uncomment the connect
+Connect-ExchangeOnline
+
+Import-Module ExchangeOnlineManagement
+
+$mailboxes = Get-Mailbox | Select-Object -Property Name
+
+$thina = @()
+
+foreach($mail in $mailboxes)
+{
+
+
+$thing = $mail -replace "@{Name=" -replace ""
+
+$thing2 = $thing -replace "}" -replace ""
+
+
+$val = Get-InboxRule -Mailbox $thing2 -ErrorAction SilentlyContinue | Select-Object -Property Name, Enabled
+
+$yourData = @(
+    [pscustomobject]@{Email=$thing2; Name='' ; Enabled=''}
+    [pscustomobject]$val
+    
+    ) 
+
+
+    Write-Host $yourData
+ 
+ $thina += $yourData 
+
+
+
+
+
+}
+
+
+$thina | Export-Csv -Path C:\MailRule.csv
+
